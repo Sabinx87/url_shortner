3bc8d0400b08ce670f5038f0d264c9c35e6be209
Sabinx87 <qsabin321@gmail.com>
2026-06-10 22:08:39 +0545

    route

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"URLSHORTNER/config"
+	"URLSHORTNER/models"
+	"URLSHORTNER/routes"
+)
+
+func main() {
+	config.ConnectDB()
+	config.DB.AutoMigrate(&models.Url{})
+
+	r := routes.SetupRouter()
+
+	r.Run()
+
+}
---
+++ routes/route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"URLSHORTNER/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/shorten", controllers.ShortenURL)
+	router.GET("/short", controllers.RedirectToOriginal)
+
+	return router
+}
